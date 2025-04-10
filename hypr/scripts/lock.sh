ce476271f50de34f86bc840effafc05f7de1b32d
crypticsaiyan <186129212+crypticsaiyan@users.noreply.github.com>
2025-04-10 13:39:20 +0530

    add hypr

---
+++ hypr/scripts/lock.sh
@@ -0,0 +1,59 @@
+# #!/bin/sh
+# swaylock \
+# 	--screenshots \
+# 	--clock \
+#   --hide-keyboard-layout \
+# 	--indicator \
+# 	--indicator-radius 100 \
+# 	--indicator-thickness 7 \
+# 	--effect-blur 7x5 \
+# 	--effect-vignette 0.5:0.5 \
+# 	--ring-color cba6f7 \
+#   --ring-ver-color 89b4fa \
+#   --ring-wrong-color f38ba8 \
+#   --ring-clear-color a6e3a1 \
+# 	--key-hl-color 1e1e2e \
+#   --bs-hl-color eba0ac \
+#   --text-color 11111b \
+#   --text-caps-lock-color 11111b \
+# 	--line-color 00000000 \
+# 	--line-ver-color 00000000 \
+# 	--line-wrong-color 00000000 \
+# 	--line-clear-color 00000000 \
+# 	--separator-color 00000000 \
+# 	--inside-color cba6f7 \
+#   --inside-ver-color 89b4fa\
+#   --inside-wrong-color f38ba8 \
+#   --inside-clear-color a6e3a1 \
+# 	--grace 2 \
+# 	--fade-in 0.2
+
+#!/bin/sh
+swaylock \
+    --screenshots \
+    --clock \
+    --hide-keyboard-layout \
+    --indicator \
+    --indicator-radius 100 \
+    --indicator-thickness 7 \
+    --effect-blur 7x5 \
+    --effect-vignette 0.5:0.5 \
+    --ring-color   #f5c2e7 \  # Pink
+    --ring-ver-color #89b4fa \  # Blue
+    --ring-wrong-color #f38ba8 \  # Red
+    --ring-clear-color #a6e3a1 \  # Green
+    --key-hl-color #f5e0dc \  # Rosewater
+    --bs-hl-color #f2cdcd \  # Flamingo
+    --text-color #cdd6f4 \  # Text
+    --text-caps-lock-color #f9e2af \  # Yellow
+    --line-color 00000000 \
+    --line-ver-color 00000000 \
+    --line-wrong-color 00000000 \
+    --line-clear-color 00000000 \
+    --separator-color 00000000 \
+    --inside-color #313244 \  # Surface0
+    --inside-ver-color #45475a \  # Surface1
+    --inside-wrong-color #f38ba8 \  # Red
+    --inside-clear-color #a6e3a1 \  # Green
+    --grace 2 \
+    --fade-in 0.2
