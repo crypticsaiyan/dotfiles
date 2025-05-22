772dc218e5890755a77519101eba90d38f2a62fe
crypticsaiyan <186129212+crypticsaiyan@users.noreply.github.com>
2025-05-22 22:48:25 +0530

    Rice more

--- nvim/lua/rahul/core/options.lua
+++ nvim/lua/rahul/core/options.lua
@@ -15,7 +15,7 @@ opt.wrap = false -- disable line wrapping
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = false -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
@@ -32,6 +32,7 @@ opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
+opt.clipboard = 'unnamedplus'
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
