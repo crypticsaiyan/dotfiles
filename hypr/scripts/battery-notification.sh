58704081c55e877ed2069051bb08d4dd13b89399
crypticsaiyan <186129212+crypticsaiyan@users.noreply.github.com>
2025-07-13 21:22:03 +0530

    Add battery notification sound

---
+++ hypr/scripts/battery-notification.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true; do
+  capacity=$(cat /sys/class/power_supply/BAT1/capacity)
+  if [ "$capacity" -lt 20 ]; then
+    mpv --no-terminal --quiet ./notification-sound.mp3
+  fi
+  sleep 30
+done
