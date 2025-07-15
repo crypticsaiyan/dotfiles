3ebc47fb5afec36001115a06647d01d061ae9657
crypticsaiyan <186129212+crypticsaiyan@users.noreply.github.com>
2025-07-15 15:17:59 +0530

    play notification only when not charging

--- hypr/scripts/battery-notification.sh
+++ hypr/scripts/battery-notification.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 while true; do
+  status=$(cat /sys/class/power_supply/BAT1/status)
   capacity=$(cat /sys/class/power_supply/BAT1/capacity)
-  if [ "$capacity" -lt 20 ]; then
+  if [ "$capacity" -lt 20 ] && [ "$status" != "Charging" ]; then
     mpv --no-terminal --quiet ~/.config/hypr/scripts/notification-sound.mp3
   fi
   sleep 30
