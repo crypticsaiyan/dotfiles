de64d7e2e92e074d90cb5f7bdb3eb3237aace89d
crypticsaiyan <186129212+crypticsaiyan@users.noreply.github.com>
2025-07-15 15:11:30 +0530

    fix battery notification sound

--- hypr/scripts/battery-notification.sh
+++ hypr/scripts/battery-notification.sh
@@ -3,7 +3,7 @@
 while true; do
   capacity=$(cat /sys/class/power_supply/BAT1/capacity)
   if [ "$capacity" -lt 20 ]; then
-    mpv --no-terminal --quiet ./notification-sound.mp3
+    mpv --no-terminal --quiet ~/.config/hypr/scripts/notification-sound.mp3
   fi
   sleep 30
 done
